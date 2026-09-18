2f9d3d0646a0f4bc8ec6aa9d39793cfa39256e5c
khongsohuyenkhung-source <khongsohuyenkhung@gmail.com>
2026-09-18 12:05:03 +0700

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,97 @@
+const CACHE_NAME = "reup-nvl-vip-v1";
+
+self.addEventListener(
+    "install",
+    function(event) {
+
+        self.skipWaiting();
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    function(event) {
+
+        event.waitUntil(
+            self.clients.claim()
+        );
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    function(event) {
+
+        const request =
+            event.request;
+
+
+        if (
+            request.method !== "GET"
+        ) {
+            return;
+        }
+
+
+        const url =
+            new URL(
+                request.url
+            );
+
+
+        if (
+            url.pathname.startsWith(
+                "/static/"
+            )
+        ) {
+
+            event.respondWith(
+
+                caches.match(
+                    request
+                ).then(
+                    function(cached) {
+
+                        if (cached) {
+                            return cached;
+                        }
+
+                        return fetch(
+                            request
+                        ).then(
+                            function(response) {
+
+                                const copy =
+                                    response.clone();
+
+                                caches.open(
+                                    CACHE_NAME
+                                ).then(
+                                    function(cache) {
+
+                                        cache.put(
+                                            request,
+                                            copy
+                                        );
+
+                                    }
+                                );
+
+                                return response;
+
+                            }
+                        );
+
+                    }
+                )
+
+            );
+
+        }
+
+    }
+);
